48445b77f40858336e175419211477b4919fe650
jinhua <jhzhu@telenavsoftware.com>
2020-12-03 13:53:15 +0800

    merge the save/load pipeline to a file

--- ev_app/pipeline_pack.py
+++ ev_app/pipeline_pack.py
@@ -1,13 +1,27 @@
 """
 @Project: Energy-Consumption   
-@Description: predict the test data, and return the value
-@Time:2020/12/1 16:02                      
+@Description: save the pipeline of preprocess, standardization, model
+@Time:2020/12/1 15:12                      
  
 """
-import os
 import joblib
-import pandas as pd
-from utils import MODEL_SAVED_PATH
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from ml_models import ModelsFitter
+from utils import *
+import os
+
+warnings.filterwarnings(action="ignore")
+
+
+def save_pipeline(model_name, data_path, X_column_names, Y_column_names, require_encode_columns, output_path):
+    train_data = clean_ev_data(data_path)
+    X = train_data[X_column_names]
+    y = train_data[Y_column_names]
+    column_transformer = get_column_transformer(require_encode_columns)
+    pipeline = make_pipeline(column_transformer, StandardScaler(), ModelsFitter(model_name))
+    pipeline.fit(X, y)
+    joblib.dump(pipeline, output_path, compress=3)
 
 
 def predict(pipeline_path, test_data_frame):
@@ -31,6 +45,14 @@ def predict(pipeline_path, test_data_frame):
 
 
 if __name__ == '__main__':
+    # save pipeline case
+    input_path = "../data/all_all_mixed_data_11_12.csv"
+    input_column_names = SPRIT_MONITOR_X_COLUMN_NAMES
+    target_column_names = SPRIT_MONITOR_TARGET_COLUMN_NAME
+    encode_column_names = SPRIT_MONITOR_REQUIRE_ENCODED_COLUMNS
+    saved_path = '%s/mix_manufactures_pipeline.joblib' % MODEL_SAVED_PATH
+    save_pipeline(RF, input_path, input_column_names, target_column_names, encode_column_names, saved_path)
+    # load pipeline case
     input_array = [['Volkswagen', 'E-Golf 300', '06.01.2019', 28, 100, 10.2, 'Summer tires', 1,	0,	1, 'Moderate',
                    24.6, 1, 1, 29]]
     header = ['manufacturer', 'version', 'fuel_date', 'trip_distance(km)', 'power(kW)', 'quantity(kWh)', 'tire_type',
